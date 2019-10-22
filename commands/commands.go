1d6a3c4b6720dc4a4937d947503efce9c1b1d873
kseals <kseals281@gmail.com>
2019-10-22 00:19:37 -0400

    Removed all test-bot commands, and references

--- commands/commands.go
+++ commands/commands.go
@@ -1,62 +1,45 @@
 package commands
 
 import (
-  "fmt"
-  "os"
+	"fmt"
 
-  "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 func errCheck(msg string, err error) {
-  if err != nil {
-    fmt.Printf("%s: %+v", msg, err)
-  }
+	if err != nil {
+		fmt.Printf("%s: %+v", msg, err)
+	}
 }
 
 func CommandHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
-  user := message.Author
-  botID := discord.State.User.ID
-  if user.ID == botID || user.Bot {
-    //Do nothing because the bot is talking
-    return
-  }
+	user := message.Author
+	botID := discord.State.User.ID
+	if user.ID == botID || user.Bot {
+		//Do nothing because the bot is talking
+		return
+	}
 
-  commandPrefix := "&"
-  content := message.Content
+	commandPrefix := "~"
+	content := message.Content
 
-  switch content {
+	switch content {
 
-    case commandPrefix + "hello":
-      discord.ChannelMessageSend(message.ChannelID, "Hello!")
+	case commandPrefix + "hello":
+		_, err := discord.ChannelMessageSend(message.ChannelID, "Hello!")
+		errCheck("Failed to send \"Hello!\" to channel", err)
 
-    case commandPrefix + "commands":
-      discord.ChannelMessageSend(message.ChannelID,
-        "__**Command List**__\n" +
-        "`hello: Test Bot returns a greeting`\n" +
-        "`gbfc: Test Bot tells you about the GBFC Overwatch team`")
+	case commandPrefix + "commands":
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"__**Command List**__\n"+
+				"`hello: Test Bot returns a greeting`\n")
+		errCheck("Failed to provide Command List to channel", err)
 
-    case commandPrefix + "oof":
-      f, err := os.Open("commands/pics/oof.png")
-      if err != nil {
-        errCheck("Something went wrong. Unable to open oof file at this time", err)
-      } else {
-        defer f.Close()
-      }
-      ms := &discordgo.MessageSend{
-        Files: []*discordgo.File{
-          &discordgo.File{
-            Name:   "commands/pics/oof.png",
-            Reader: f,
-          },
-        },
-      }
-      discord.ChannelMessageSendComplex(message.ChannelID, ms)
+	default:
+		{
+		} // Do nothing
 
-    default: {} // Do nothing
+	}
 
-    }
-
-  fmt.Printf("Message: %+v || From: %s\n\n", message.Message, message.Author)
+	fmt.Printf("Message: %+v || From: %s\n\n", message.Message, message.Author)
 }
-
-func
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "log"
-  "os"
-  "os/signal"
-  "syscall"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-  "test-bot/commands"
+	"ToadsLeaderboards/commands"
 
-  "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 // Session is declared in the global space so it can be easily used
@@ -18,67 +18,60 @@ import (
 // In this use case, there is no error that would be returned.
 var Session, _ = discordgo.New()
 
-var (
-    commandPrefix string
-    botID         string
-)
-
 // Read in all options from environment variables and command line arguments.
 func init() {
 
-  // Discord Authentication Token
-  Session.Token = os.Getenv("DISCORD_TEST_BOT")
-  if Session.Token == "" {
-    // Pointer, flag, default, description
-    flag.StringVar(&Session.Token, "t", "", "Discord Authentication Token")
-  }
+	// Discord Authentication Token
+	Session.Token = os.Getenv("DISCORD_TOAD_BOT")
+	if Session.Token == "" {
+		// Pointer, flag, default, description
+		flag.StringVar(&Session.Token, "t", "", "Discord Authentication Token")
+	}
 }
 
 func main() {
 
-  // Declare any variables needed later.
-  var err error
+	// Declare any variables needed later.
+	var err error
 
-  // Setup interrupt
-  interrupt := make(chan os.Signal)
-  signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	// Setup interrupt
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 
-  // Parse command line arguments
-  flag.Parse()
+	// Parse command line arguments
+	flag.Parse()
 
-  // Verify a Token was provided
-  if Session.Token == "" {
-    log.Println("You must provide a Discord authentication token.")
-    return
-  }
+	// Verify a Token was provided
+	if Session.Token == "" {
+		log.Println("You must provide a Discord authentication token.")
+		return
+	}
 
-  // Verify the Token is valid and grab user information
-  Session.State.User, err = Session.User("@me")
-  errCheck("error retrieving account", err)
+	// Verify the Token is valid and grab user information
+	Session.State.User, err = Session.User("@me")
+	errCheck("error retrieving account", err)
 
-  botID = Session.State.User.ID
-  Session.AddHandler(commands.CommandHandler)
-  Session.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
-    err = discord.UpdateStatus(0, "A friendly development bot!")
-    if err != nil {
-      fmt.Println("Error attempting to set my status")
-    }
-    servers := discord.State.Guilds
-    fmt.Printf("Test-Bot has started on %d servers", len(servers))
-  })
+	Session.AddHandler(commands.CommandHandler)
+	Session.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
+		err = discord.UpdateStatus(0, "A friendly leaderboard!")
+		if err != nil {
+			fmt.Println("Error attempting to set my status")
+		}
+		servers := discord.State.Guilds
+		fmt.Printf("Toad's Leaderboard has started on %d servers\n", len(servers))
+	})
 
-  // Open a websocket connection to Discord
-  err = Session.Open()
-  defer Session.Close()
-  errCheck("Error opening connection to Discord", err)
+	// Open a websocket connection to Discord
+	err = Session.Open()
+	errCheck("Error opening connection to Discord", err)
+	defer Session.Close()
 
-  <-interrupt
+	<-interrupt
 }
 
 func errCheck(msg string, err error) {
-  if err != nil {
-    fmt.Printf("%s: %+v", msg, err)
-    panic(err)
-  }
+	if err != nil {
+		fmt.Printf("%s: %+v", msg, err)
+		panic(err)
+	}
 }
-
