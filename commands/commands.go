ba600a813f96e99de135e517eab5b9d7796423e0
Khari Seals <kseals281@gmail.com>
2019-11-06 12:31:50 -0500

    Direct messages sent to participants of the rps game are now checked for the rps reactions. Error checking was also added to all message sends

--- commands/commands.go
+++ commands/commands.go
@@ -1,57 +1,66 @@
 package commands
 
 import (
-  "fmt"
-  "os"
-  s "strings"
+	"fmt"
+	"os"
+	s "strings"
 
-  "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 func errCheck(msg string, err error) {
-  if err != nil {
-    fmt.Printf("%s: %+v", msg, err)
-  }
+	if err != nil {
+		_ = fmt.Errorf("%s: %+v", msg, err)
+	}
 }
 
 func CommandHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
-  user := message.Author
-  botID := discord.State.User.ID
-  if user.ID == botID || user.Bot {
-    //Do nothing because the bot is talking
-    return
-  }
+	user := message.Author
+	botID := discord.State.User.ID
+	if user.ID == botID || user.Bot {
+		//Do nothing because the bot is talking
+		return
+	}
 
-  commandPrefix := "&"
-  content := message.Content
+	commandPrefix := "&"
+	content := message.Content
 
+	switch {
 
-  if s.HasPrefix(content, commandPrefix + "hello") {
-    discord.ChannelMessageSend(message.ChannelID, "Hello!")
-  } else if s.HasPrefix(content, commandPrefix + "commands") {
-    discord.ChannelMessageSend(message.ChannelID,
-      "__**Command List**__\n" +
-      "`hello: Test Bot returns a greeting`\n" +
-      "`oof: Test Bot replies with a big oof`")
-  } else if s.HasPrefix(content, commandPrefix + "oof") {
-    f, err := os.Open("commands/pics/oof.png")
-    if err != nil {
-      errCheck("Something went wrong. Unable to open oof file at this time", err)
-    } else {
-      defer f.Close()
-    }
-    ms := &discordgo.MessageSend{
-      Files: []*discordgo.File{
-        &discordgo.File{
-          Name:   "commands/pics/oof.png",
-          Reader: f,
-        },
-      },
-    }
-    discord.ChannelMessageSendComplex(message.ChannelID, ms)
-  } else if s.HasPrefix(content, commandPrefix + "rps") {
-    RPSHandler(discord, message)
-  }
+	case s.HasPrefix(content, commandPrefix+"hello"):
+		_, err := discord.ChannelMessageSend(message.ChannelID, "Hello!")
+		errCheck("", err)
 
-  fmt.Printf("Message: %+v || From: %s\n\n", message.Message, message.Author)
+	case s.HasPrefix(content, commandPrefix+"commands"):
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"__**Command List**__\n"+
+				"`hello: Test Bot returns a greeting`\n"+
+				"`oof: Test Bot replies with a big oof`")
+		errCheck("Failed to send list of commands", err)
+
+	case s.HasPrefix(content, commandPrefix+"oof"):
+		f, err := os.Open("commands/pics/oof.png")
+		if err != nil {
+			errCheck("Something went wrong. Unable to open oof file at this time", err)
+		} else {
+			defer f.Close()
+		}
+		ms := &discordgo.MessageSend{
+			Files: []*discordgo.File{
+				{
+					Name:   "commands/pics/oof.png",
+					Reader: f,
+				},
+			},
+		}
+		_, err = discord.ChannelMessageSendComplex(message.ChannelID, ms)
+		if err != nil {
+			errCheck("Unable to send oof to channel", err)
+		}
+
+	case s.HasPrefix(content, commandPrefix+"rps"):
+		RPSHandler(discord, message)
+	}
+
+	fmt.Printf("Message: %+v || From: %s\n\n", message.Message, message.Author)
 }
--- commands/rps.go
+++ commands/rps.go
@@ -1,89 +1,142 @@
 package commands
 
 import (
-  // "fmt"
-  // "os"
-  // s "strings"
-  // "sync"
-
-  "github.com/bwmarrin/discordgo"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"sync"
+	"time"
 )
 
 func RPSHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
-  // Webhook messages do not contain a full author (i.e. non-user author) and
-  // should be ignored as a result
-  if message.WebhookID != "" {
-    return
-  }
+	// Webhook messages do not contain a full author (i.e. non-user author) and
+	// should be ignored as a result
+	if message.WebhookID != "" {
+		return
+	}
 
-  if message.MentionEveryone {
-    discord.ChannelMessageSend(message.ChannelID,
-      "You may not challenge everyone to a game of rock, paper, scissors")
-    return
-  } else if len(message.MentionRoles) > 0 {
-    discord.ChannelMessageSend(message.ChannelID,
-      "You may not challenge a role to a game of rock, paper, scissors")
-    return
-  } else if len(message.Mentions) > 1 {
-    discord.ChannelMessageSend(message.ChannelID,
-      "You can only challenge one person at a time to rock, paper, scissors")
-    return
-  } else if len(message.Mentions) == 0 {
-    discord.ChannelMessageSend(message.ChannelID,
-      "You must select at least one opponent for rock, paper, scissors")
-    return
-  }
-  // TODO: Add check for user challenging themselves
+	if message.MentionEveryone {
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"You may not challenge everyone to a game of rock, paper, scissors")
+		if err != nil {
+			errCheck("Unable to sent cannot challenge everyone message", err)
+		}
+		return
+	} else if len(message.MentionRoles) > 0 {
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"You may not challenge a role to a game of rock, paper, scissors")
+		if err != nil {
+			errCheck("Error sending role challenge denial", err)
+		}
+		return
+	} else if len(message.Mentions) > 1 {
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"You can only challenge one person at a time to rock, paper, scissors")
+		errCheck("Unable to send only one opponent message", err)
+		return
+	} else if len(message.Mentions) == 0 {
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"You must select at least one opponent for rock, paper, scissors")
+		errCheck("Unable to send at least one opponent error", err)
+		return
+	} else if message.Mentions[0].ID == message.Author.ID {
+		_, err := discord.ChannelMessageSend(message.ChannelID,
+			"You cannot challenge yourself to a game of rock paper scissors")
+		if err != nil {
+			errCheck("Unable to send no challenging yourself message", err)
+		}
+	}
+	// TODO: Add timeout for player choices
 
-  dmChannels := make(chan []*discordgo.Channel)
-  // TODO: Add timeout for player choices
+	p1, p2 := rpsContactPlayers(discord, message)
+	if p2 == nil {
+		errCheck("Error Contacting players", nil)
+	}
 
-  go rpsContactPlayers(discord, message, dmChannels)
+	var wg sync.WaitGroup
+	reactions := make(chan *discordgo.MessageReaction)
+	wg.Add(2)
+	go rpsWaitForReaction(&wg, discord, p1, reactions)
+	go rpsWaitForReaction(&wg, discord, p2, reactions)
 
-  // discord.AddHandlerOnce(rpsGetReaction)
-
+	wg.Wait()
+	fmt.Println(<-reactions)
+	fmt.Println(<-reactions)
 }
 
-func rpsContactPlayers(discord *discordgo.Session, message *discordgo.MessageCreate, dmChannels chan []*discordgo.Channel) {
-  player_2 := message.Mentions[0]
-  player_1 := message.Author
+func rpsContactPlayers(discord *discordgo.Session, message *discordgo.MessageCreate) (*discordgo.Message, *discordgo.Message) {
+	player2 := message.Mentions[0]
+	player1 := message.Author
 
-  dmP1, err := discord.UserChannelCreate(player_1.ID)
-  if err != nil {
-    errCheck("Unable to create direct message with the challenger", err)
-    return
-  }
-  dmP1.Name = player_1.Username + "1"
+	dmP1, err := discord.UserChannelCreate(player1.ID)
+	if err != nil {
+		errCheck("Unable to create direct message with the challenger", err)
+		return nil, nil
+	}
+	dmP1.Name = player1.Username + "1"
 
-  dmP2, err := discord.UserChannelCreate(player_2.ID)
-  if err != nil {
-    errCheck("Unable to create direct message with the opponent", err)
-    return
-  }
-  dmP2.Name = player_2.Username + "2"
+	dmP2, err := discord.UserChannelCreate(player2.ID)
+	if err != nil {
+		errCheck("Unable to create direct message with the opponent", err)
+		return nil, nil
+	}
+	dmP2.Name = player2.Username + "2"
 
-  p1Message := "__**You have challenged " + player_2.Username + " to a game of rock paper scissors!!!**__\n"
-  p2Message := "__**" + player_1.Username + " has challenged you to a match of rock paper scissors!!!**__\n"
+	p1Message := "__**You have challenged " + player2.Username + " to a game of rock paper scissors!!!**__\n"
+	p2Message := "__**" + player1.Username + " has challenged you to a match of rock paper scissors!!!**__\n"
 
-  commonMessage := (":fist::raised_hand::v:\t:fist::raised_hand::v:\t:fist::raised_hand::v:\t" +
-              ":fist::raised_hand::v:\t:fist::raised_hand::v:" +
-              "\n\n" +
-              "Rules: You must react to this message with either the rock (:fist:)," +
-              " paper (:raised_hand:), or scissors (:v:) emoji.\n" +
-              "- Rock beats scissors\n" +
-              "- Scissors beats paper\n" +
-              "- Paper beats rock")
+	commonMessage := ":fist::raised_hand::v:\t:fist::raised_hand::v:\t:fist::raised_hand::v:\t" +
+		":fist::raised_hand::v:\t:fist::raised_hand::v:" +
+		"\n\n" +
+		"Rules: You must react to this message with either the rock (:fist:)," +
+		" paper (:raised_hand:), or scissors (:v:) emoji.\n" +
+		"- Rock beats scissors\n" +
+		"- Scissors beats paper\n" +
+		"- Paper beats rock"
 
-  discord.ChannelMessageSend(dmP1.ID, (p1Message + commonMessage))
-  discord.ChannelMessageSend(dmP2.ID, (p2Message + commonMessage))
-
-  dm := make([]*discordgo.Channel, 2)
-  dm[0] = dmP1
-  dm[1] = dmP2
-  dmChannels <- dm
-
+	p1, err := discord.ChannelMessageSend(dmP1.ID, p1Message+commonMessage)
+	if err != nil {
+		errCheck("Error sending message to challenger", err)
+		return nil, nil
+	}
+	p2, err := discord.ChannelMessageSend(dmP2.ID, p2Message+commonMessage)
+	if err != nil {
+		errCheck("Error sending message to opponent", err)
+		return nil, nil
+	}
+	return p1, p2
 }
 
-// func rpsGetReaction(discord *discordgo.Session, dmChannels *discordgo.Channel) {
-//   <- dmChannels
-// }
+func rpsWaitForReaction(wg *sync.WaitGroup, s *discordgo.Session, message *discordgo.Message, reactions chan *discordgo.MessageReaction) {
+	// TODO: Do this in a more efficient manner
+	if len(message.Reactions) > 0 {
+		_ = s.MessageReactionsRemoveAll(message.ChannelID, message.ID)
+	}
+	limiter := time.Tick(1000 * time.Millisecond)
+	choices := [3]string{"✊", "✋", "✌"}
+	for i := 0; i < 500; i++ {
+		message, _ = s.ChannelMessage(message.ChannelID, message.ID) // Refresh message
+		if len(message.Reactions) == 0 {
+			continue
+		}
+		// TODO: Remove all non rps reactions
+		fmt.Printf("Tick %d\tReactions: %v\n", i, message.Reactions[0].Emoji)
+		for _, emoji := range choices {
+			users, err := s.MessageReactions(message.ChannelID, message.ID, emoji, 1)
+			errCheck("Error getting number of users who reacted", err)
+			if len(users) > 0 {
+				fmt.Println("GOT A REACTION")
+				mr := messageReaction(message.Reactions[0], message)
+				reactions <- mr
+				wg.Done()
+				return
+			}
+		}
+		<-limiter
+	}
+}
+
+func messageReaction(reaction *discordgo.MessageReactions, message *discordgo.Message) *discordgo.MessageReaction {
+	mr := discordgo.MessageReaction{UserID: message.Author.ID, MessageID: message.ID, Emoji: *reaction.Emoji,
+		ChannelID: message.ChannelID, GuildID: message.GuildID}
+	return &mr
+}
